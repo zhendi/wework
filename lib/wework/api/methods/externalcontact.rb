fd636d0cafa848a601e96bb763416e2f496e1573
zhendi <zhendi@seeapp.com>
2021-01-08 00:03:13 +0800

    feat: get_contact_way

--- lib/wework/api/methods/externalcontact.rb
+++ lib/wework/api/methods/externalcontact.rb
@@ -4,6 +4,14 @@ module Wework
   module Api
     module Methods
       module Externalcontact
+        def get external_userid
+          get 'externalcontact/get', params: {external_userid: external_userid}
+        end
+
+        def get_contact_way config_id
+          get 'externalcontact/get_contact_way', params: {config_id: config_id}
+        end
+
         def add_contact_way type, scene, state, user
           # https://work.weixin.qq.com/api/doc/90001/90143/92577
           payload = {type: type, scene: scene, state: state, user: user}
